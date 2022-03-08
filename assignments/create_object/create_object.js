ac94a8eedb0ca13e06a1bd6b2b275a8ff6c03903
Rahul-Biswal <rahulbiswalnew@gmail.com>
2022-03-08 23:18:05 +0530

    can drive submission

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,14 @@
 function CreateObject(arr) {
     // Write your code here
-}
+    var obj = {};
+    for (var i=0; i<arr.length-1; i=i+2){
+        obj[arr[i]]=arr[i+1]
+    }
+    return obj
 
+}
+Yash 
+
+// ["firstName", "Yash", "lastName": "Goyal"]
 module.exports = CreateObject;
+
